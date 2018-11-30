7a1e608bec0d0f9b620501876aef1a374e6d7389
delroid <delroy.mitchell@gmail.com>
2018-11-30 12:24:51 +0000

    Add files via upload

---
+++ addnewnic.ps1
@@ -0,0 +1,11 @@
+﻿
+#list of vms to add a new nic 
+$vms=get-content F:\powershell\vmware\vm.txt
+ 
+#add the following nic to each vm in array $vm
+foreach ($vm in $vms){
+ 
+    foreach ($i in 1){ 
+    new-networkadapter -vm $vm -Portgroup "PRODUCTION" -Type "VMXNET3" 
+                     }
+             }
